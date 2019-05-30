873600434d27e7b4b0cb59b38f9eeb81a3c33c8e
jannmoil <46961456+jannmoil@users.noreply.github.com>
2019-05-30 17:56:14 +0300

    Add files via upload

---
+++ Management.php
@@ -0,0 +1,10 @@
+<?php
+$title = "Management";
+
+$content = '<h3>Coffee</h3>
+            <a href="CoffeeAdd.php">Add a new coffee</a><br/>
+            <a href="">Upload Image</a><br/>
+            <a href="">Coffee Overview</a><br/>';
+
+include './Template.php';
+?>

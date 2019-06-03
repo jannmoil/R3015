11c881228496ad50b0235c22422b2de89a1ed800
jannmoil <46961456+jannmoil@users.noreply.github.com>
2019-06-03 15:34:48 +0300

    Add files via upload

--- Management.php
+++ Management.php
@@ -2,7 +2,7 @@
 $title = "Management";
 
 $content = '<h3>Coffee</h3>
-            <a href="CoffeeAdd.php">Add a new coffee</a><br/>
+            <a href="ProductAdd.php">Add a new coffee</a><br/>
             <a href="">Upload Image</a><br/>
             <a href="">Coffee Overview</a><br/>';
 

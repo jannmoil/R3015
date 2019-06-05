803a0f64d03add9f4fa2a65111a5a9f7858d7d4b
jannmoil <46961456+jannmoil@users.noreply.github.com>
2019-06-05 10:31:27 +0300

    Update ProductAdd.php

--- ProductAdd.php
+++ ProductAdd.php
@@ -12,30 +12,30 @@ $content ="<form action='insert.php' method='post'>
     <fieldset>
         <legend>Add a new product</legend>
         <label for='name'>Name: </label>
-        <input type='text' class='inputField' name='txtName' /><br/>
+        <input type='text' class='inputField' name='txtName' id='txtName' /><br/>
 
         <label for='type'>Type: </label>
-        <select class='inputField' name='ddlType'>
+        <select class='inputField' name='ddlType' id='ddlType'>
             <option value='%'>All</option>"
         .$siteController->CreateOptionValues($siteController->GetProductTypes()).
         "</select><br/>
 
         <label for='price'>Price: </label>
-        <input type='text' class='inputField' name='txtPrice' /><br/>
+        <input type='text' class='inputField' name='txtPrice' id='txtPrice' /><br/>
 
         <label for='color'>Color: </label>
-        <input type='text' class='inputField' name='txtColor' /><br/>
+        <input type='text' class='inputField' name='txtColor' id='txtColor'/><br/>
 
         <label for='label'>Label: </label>
-        <input type='text' class='inputField' name='txtLabel' /><br/>
+        <input type='text' class='inputField' name='txtLabel' id='txtLabel' /><br/>
 
         <label for='image'>Image: </label>
-        <select class='inputField' name='ddlImage'>"
+        <select class='inputField' name='ddlImage' id='ddlImage'>"
         .$siteController->GetImages().
         "</select></br>
 
         <label for='review'>Review: </label>
-        <textarea cols='70' rows='12' name='txtReview'></textarea></br>
+        <textarea cols='70' rows='12' name='txtReview' id='txtReview'></textarea></br>
 
         <input type='submit' value='Submit'>
     </fieldset>

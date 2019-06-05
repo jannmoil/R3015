a504792bb18785e84bef4497e370db176c75d125
jannmoil <46961456+jannmoil@users.noreply.github.com>
2019-06-05 14:56:48 +0300

    Update Management.php

--- Management.php
+++ Management.php
@@ -4,7 +4,7 @@ $title = "Management";
 $content = '<h3>Product Management</h3>
             <a href="ProductAdd.php">Add a new product</a><br/>
             <a href="">Upload Image</a><br/>
-            <a href="">Product Overview</a><br/>';
+            <a href="ProductOverview.php">Product Overview</a><br/>';
 
 include './Template.php';
 ?>

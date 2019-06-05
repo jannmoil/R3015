ad481f0d83f68c51c56f2f56780d3ca24a3e0de0
jannmoil <46961456+jannmoil@users.noreply.github.com>
2019-06-05 11:05:45 +0300

    Update insert.php

--- insert.php
+++ insert.php
@@ -17,13 +17,13 @@ if($conn->connect_error) {
     die("Connection failed:" . $conn->connect_error);
 }
 
-$txtName = mysqli_real_escape_string($conn, $_POST['name']);
-$ddlType = mysqli_real_escape_string($conn, $_POST['type']);
-$txtPrice = mysqli_real_escape_string($conn, $_POST['price']);
-$txtColor = mysqli_real_escape_string($conn, $_POST['color']);
-$txtLabel = mysqli_real_escape_string($conn, $_POST['label']);
-$ddlImage = mysqli_real_escape_string($conn, $_POST['image']);
-$txtReview = mysqli_real_escape_string($conn, $_POST['review']);
+$txtName = mysqli_real_escape_string($conn, $_POST['txtName']);
+$ddlType = mysqli_real_escape_string($conn, $_POST['ddlType']);
+$txtPrice = mysqli_real_escape_string($conn, $_POST['txtPrice']);
+$txtColor = mysqli_real_escape_string($conn, $_POST['txtColor']);
+$txtLabel = mysqli_real_escape_string($conn, $_POST['txtLabel']);
+$ddlImage = mysqli_real_escape_string($conn, $_POST['ddlImage']);
+$txtReview = mysqli_real_escape_string($conn, $_POST['txtReview']);
 
 $sql = "INSERT INTO product (name, type, price, color, label, image, review)
          VALUES ('$txtName', '$ddlType', '$txtPrice', '$txtColor', '$txtLabel', '$ddlImage', '$txtReview')";

845c08df7188c3b58913a0a17bf4014ce124c7f4
jannmoil <46961456+jannmoil@users.noreply.github.com>
2019-06-06 15:05:16 +0300

    Update Template.php

--- Template.php
+++ Template.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title><?php echo $title; ?></title>

76612d155fa8b734d481475fdb34100692c3652b
jannmoil <46961456+jannmoil@users.noreply.github.com>
2019-06-06 14:22:27 +0300

    Update Management.php

--- Management.php
+++ Management.php
@@ -3,8 +3,7 @@ $title = "Management";
 
 $content = '<h3>Product Management</h3>
             <a href="ProductAdd.php">Add a new product</a><br/>
-            <a href="">Upload Image</a><br/>
-            <a href="ProductOverview.php">Product Overview</a><br/>';
+            <a href="delete_records.php">Delete products</a><br/>';
 
 include './Template.php';
 ?>
